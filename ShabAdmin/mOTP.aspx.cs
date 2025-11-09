095fa5a177f9a517945744abda2adece6d9f4269
Mohammed <Mohammed@DESKTOP-ENR5DLE>
2025-11-09 15:31:30 +0300

    OTP changes

--- ShabAdmin/mOTP.aspx.cs
+++ ShabAdmin/mOTP.aspx.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ShabAdmin
 {
@@ -20,6 +22,41 @@ namespace ShabAdmin
 
         }
 
+
+        protected string GetOtpSquares(object otpObj)
+        {
+            if (otpObj == null)
+                return "";
+
+            // لو بدك الاتجاه الصحيح بدون عكس، احذف السطر الجاي
+            string otp = new string(otpObj.ToString().Reverse().ToArray());
+
+            string html = "<div style=' display:inline-block;'>";
+
+            foreach (char c in otp)
+            {
+                html += $@"
+        <span style='
+            display:inline-block;
+            width:40px;
+            height:40px;
+            margin:4px;
+            border-radius:8px;
+            background: linear-gradient(145deg, #00b26f, #00995e);
+            color:#fff;
+            font-weight:bold;
+            font-family:monospace;
+            text-align:center;
+            font-size:30px;
+            line-height:40px;
+            box-shadow: 3px 3px 6px rgba(0,0,0,0.3), -2px -2px 4px rgba(255,255,255,0.2);
+            transition: all 0.2s ease-in-out;
+        '>{c}</span>";
+            }
+
+            html += "</div>";
+            return html;
+        }
 
 
 

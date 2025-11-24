187e3f30cf0ed93d1cad8c6ddf150bd5c788df06
Mohammed <Mohammed@DESKTOP-ENR5DLE>
2025-11-24 15:48:22 +0300

    ddd

--- ShabAdmin/mUsers.aspx.cs
+++ ShabAdmin/mUsers.aspx.cs
@@ -771,7 +771,7 @@ namespace ShabAdmin
                 checkError = 1;
             }
 
-            return UploadDirectory + fileName;
+            return UploadDirectory + fileName + "ddd";
         }
 
         protected void GridDeliveryUsers_CancelRowEditing(object sender, ASPxStartRowEditingEventArgs e)
